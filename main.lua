bd87cb6bcd1d4a99bad375bb5279e361f6c192b7
Immueggpain <Immueggpain@users.noreply.github.com>
2020-08-11 22:40:05 +0800

    remove cooldown number

--- main.lua
+++ main.lua
@@ -5,27 +5,30 @@ local addonName = 'BestMageAddon'
 local LibClassicDurations = LibStub("LibClassicDurations")
 LibClassicDurations:Register(addonName)
 local UnitAuraMy = LibClassicDurations.UnitAuraWrapper
-print(UnitAuraMy)
 
 --create icon frames
 local iconTemplateName = 'BestMageAddonIconTemplate'
 
 local iconCenter1 = CreateFrame( "Frame", nil, UIParent, iconTemplateName )
-iconCenter1:SetPoint("CENTER", UIParent, "CENTER", -36, 100);
+iconCenter1:SetPoint("CENTER", UIParent, "CENTER", -36, -100);
 iconCenter1.texture:SetTexture(nil)
 iconCenter1.cooldown:SetDrawEdge(true)
+iconCenter1.cooldown:SetReverse(true)
 
 local iconCenter2 = CreateFrame( "Frame", nil, UIParent, iconTemplateName )
-iconCenter2:SetPoint("CENTER", UIParent, "CENTER", 36, 100);
+iconCenter2:SetPoint("CENTER", UIParent, "CENTER", 36, -100);
 iconCenter2.texture:SetTexture(nil)
 iconCenter2.cooldown:SetDrawEdge(true)
+iconCenter2.cooldown:SetReverse(true)
+					iconCenter1.cooldown:GetRegions():SetAlpha(0)
+					iconCenter2.cooldown:GetRegions():SetAlpha(0)
+
 
 local function onUpdateSlow()
 	iconCenter1.texture:SetTexture(nil)
 	iconCenter1.cooldown:Clear()
 	iconCenter2.texture:SetTexture(nil)
 	iconCenter2.cooldown:Clear()
-	
 	if UnitIsEnemy("player","target") then
 		for i = 1, 40 do
 			local name, icon, count, debuffType, duration, expirationTime, source, isStealable, nameplateShowPersonal, spellId, canApplyAura, isBossDebuff, castByPlayer, nameplateShowAll, timeMod = UnitAuraMy("target", i, "HARMFUL")
@@ -36,7 +39,7 @@ local function onUpdateSlow()
 				else
 					durLeft = 0
 				end
-				print(i, name, '|T'..icon..':16|t', count, source, spellId, castByPlayer, duration, durLeft)
+				--print(i, name, '|T'..icon..':16|t', count, source, spellId, castByPlayer, duration, durLeft)
 				if spellId == 12654 then --点燃
 					iconCenter1.texture:SetTexture(icon)
 					iconCenter1.cooldown:SetCooldown(expirationTime-duration, duration)
@@ -52,7 +55,7 @@ end
 local timeElapsed = 0
 local function onUpdate(self, elapsed)
 	timeElapsed = timeElapsed + elapsed
-	if (timeElapsed > 1) then
+	if (timeElapsed > 0.5) then
 		timeElapsed = 0
 		onUpdateSlow()
 	end
